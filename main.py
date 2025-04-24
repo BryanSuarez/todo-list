b1d3d5de5a17cbfde833e69d80b1ea4394951f1a
BryanSuarez <bryansuarez@example.com>
2025-04-23 22:38:14 -0500

    add new endpoint

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"message": "Hello World"}
+
+
+# create a GET endpoint to list tasks
+@app.get("/tasks")
+async def list_tasks():
+    return {"message": "Tasks listed"}
